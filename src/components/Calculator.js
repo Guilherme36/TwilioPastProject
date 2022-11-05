493f4feb401ca9d18a1dd5035d4375ca63dba542
Guilherme Felipe <50681430+Guilherme36@users.noreply.github.com>
2022-11-05 14:01:06 -0300

    Initial commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,19 @@
+import { Card } from '@twilio-paste/core';
+import { MenuItemsContainer } from '../containers/MenuItemsContainer';
+import { TipSelectContainer } from '../containers/TipSelectContainer';
+import { NewItemFormContainer } from '../containers/NewItemFormContainer';
+
+import { Summary } from './Summary';
+
+const Calculator = () => {
+  return (
+    <Card>
+      <NewItemFormContainer />
+      <MenuItemsContainer />
+      <TipSelectContainer />
+      <Summary />
+    </Card>
+  );
+};
+
+export default Calculator;
